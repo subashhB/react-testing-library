c7bcdb76b3f9478df7bc4503e779c6259e537794
Admin <subashbaidya9@gmail.com>
2023-06-24 18:28:25 +0545

    refactor - Descibe Block

--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -10,42 +10,19 @@ const MockTodoFooter = ({numberOfIncompleteTasks})=>{
         </BrowserRouter>
     )
 }
+describe("ToDoFooter", ()=>{
+    it("should render the correct amount of incomplete tasks",async()=>{
+        // ! Testing <TodoFooter/> component in isolation would not work because <Link> is being used inside the components and it will not work without <BrowserRouter> which is inside <App/> component.
+        // render(<TodoFooter numberOfIncompleteTasks={5} />); 
+        render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+        const paragraphElement = screen.getByText(/5 tasks left/i);
+        expect(paragraphElement).toBeInTheDocument();
+    });
+    
+    it("should render 'task' when there is only one incomplete task left",async()=>{
+        render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+        const paragraphElement = screen.getByText(/1 task left/i);
+        expect(paragraphElement).toBeInTheDocument();
+    });  
+})
 
-it("should render the correct amount of incomplete tasks",async()=>{
-    // ! Testing <TodoFooter/> component in isolation would not work because <Link> is being used inside the components and it will not work without <BrowserRouter> which is inside <App/> component.
-    // render(<TodoFooter numberOfIncompleteTasks={5} />); 
-    render(<MockTodoFooter numberOfIncompleteTasks={5} />);
-    const paragraphElement = screen.getByText(/5 tasks left/i);
-    expect(paragraphElement).toBeInTheDocument();
-});
-
-it("should render 'task' when there is only one incomplete task left",async()=>{
-    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-    const paragraphElement = screen.getByText(/1 task left/i);
-    expect(paragraphElement).toBeInTheDocument();
-});
-
-it("should render 'task' when there is only one incomplete task left (toBeVisible)",async()=>{
-    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-    const paragraphElement = screen.getByText(/1 task left/i);
-    expect(paragraphElement).toBeVisible(); //Fails when an element is hidden or opacity is 0
-});
-
-it("should render 'task' when there is only one incomplete task left (toContainHTML)",async()=>{
-    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-    const paragraphElement = screen.getByText(/1 task left/i);
-    expect(paragraphElement).toContainHTML("p"); //Contains a <p> element
-});
-
-it("should render 'task' when there is only one incomplete task left (toHaveTextContent)",async()=>{
-    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-    const paragraphElement = screen.getByTestId("para");
-    expect(paragraphElement).toHaveTextContent("1 task left");
-});
-
-it("should render 'task' when there is only one incomplete task left (toBe)",async()=>{
-    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-    const paragraphElement = screen.getByTestId("para");
-    //*Getting some attributes from the element
-    expect(paragraphElement.textContent).toBe("1 task left"); //Use element.value for input value
-});
